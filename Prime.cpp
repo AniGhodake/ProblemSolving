7865790b2469332d85d4d8e9f3e9f1680886b14f
AniketGhodake <ghodakeaniket27@gmail.com>
2023-09-24 00:01:55 +0530

    24 Sept 2023

---
+++ Prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+bool Prime(int num){
+    for(int start=2;start<num;start++){
+        if(num%start==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int num;
+    cout<<"Enter the Number"<<endl;
+    cin>>num;
+    bool result = Prime(num);
+    if(result){
+        cout<<"Entered Number "<<num<<" is prime number";
+    }
+    else{
+        cout<<"Entered Number "<<num<<" is not a prime number";
+    }
+}
