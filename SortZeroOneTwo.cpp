14b89a9da8b767a686803d4dbf01ce69306f4b97
AniketGhodake <ghodakeaniket27@gmail.com>
2023-12-16 21:18:12 +0530

    16 Dec 2023

--- SortZeroOneTwo.cpp
+++ SortZeroOneTwo.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void sortZOT(int arr[], int size){
-    for(int i = 0,left = 0, right = size-1; i < size, left<=right; ){
+void sortZOT(int arr[],int size){
+    for(int i = 0,left = 0, right = size-1; i <=right && left<=right; ){
         if(arr[i]==0){
             swap(arr[left],arr[i]);
             left++;
             i++;
         }
-        if(arr[i]==2){
+        else if(arr[i]==2){                    // doubt==========> remove else keyword from this line and then run
             swap(arr[right],arr[i]);                       
             right--;
             
@@ -16,10 +16,8 @@ void sortZOT(int arr[], int size){
         else{
             i++;
         }
-
     }
-
-    cout<<"Array elements after sorting is as follws : "<<endl;
+    cout<<"Array elements after sorting is as follows : "<<endl;
     for(int i = 0; i < size; i++){
         cout<<arr[i]<<" ";
     }
@@ -33,6 +31,11 @@ int main(){
     for (int i = 0; i < size; i++){
         cin>>arr[i];
     }
+    cout<<"Array elements before sorting is as follows : "<<endl;
+    for(int i = 0; i < size; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<< endl;
 
     sortZOT(arr,size);
 }
